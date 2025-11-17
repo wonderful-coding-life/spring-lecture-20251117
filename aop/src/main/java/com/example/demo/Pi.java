71fafb6dcb1c68997b99b3f5b547954728af4204
spiderman <spiderman@xhift.co.kr>
2025-11-17 15:40:24 +0900

    no message

--- aop/src/main/java/com/example/demo/Pi.java
+++ aop/src/main/java/com/example/demo/Pi.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Pi {
-    public double calculate(int points) {
+    @PrintExecutionTime
+    public double calculatePi(int points) {
         int circle = 0;
         for (long i = 0; i < points; i++) {
             double x = Math.random() * 2 - 1;
@@ -11,5 +15,9 @@ public class Pi {
             }
         }
         return 4.0 * circle / points;
+    }
+
+    public double calculate(int points) {
+        return calculatePi(points);
     }
 }
