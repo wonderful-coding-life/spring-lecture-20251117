4829b3f4b1d97ed8d603d1559b24980efa62f590
spiderman <spiderman@xhift.co.kr>
2025-11-19 12:22:40 +0900

    no message

--- rest/src/main/java/com/example/demo/controller/MemberController.java
+++ rest/src/main/java/com/example/demo/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.NotFoundException;
 import com.example.demo.model.Member;
 import com.example.demo.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,18 +15,49 @@ public class MemberController {
     private MemberRepository memberRepository;
 
     @PostMapping("/members")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Member postMembers(@RequestBody Member member) {
         memberRepository.save(member);
         return member;
     }
 
     @GetMapping("/members")
-    public List<Member> getMembers() {
-        return memberRepository.findAll();
+    public List<Member> getMembers(@RequestParam(name="name", required = false) String name) {
+        if (name == null || name.isBlank()) {
+            return memberRepository.findAll();
+        } else {
+            return memberRepository.findByName(name);
+        }
     }
 
     @GetMapping("/members/{id}")
-    public Member getMemberById(@PathVariable("id") Long id) {
-        return memberRepository.findById(id).orElseThrow();
+    public Member getMembersById(@PathVariable("id") Long id) {
+        return memberRepository.findById(id).orElseThrow(NotFoundException::new);
+    }
+
+    @PutMapping("/members/{id}")
+    public Member putMembers(@PathVariable("id") Long id, @RequestBody Member member) {
+        Member me =  memberRepository.findById(id).orElseThrow(NotFoundException::new);
+        me.setName(member.getName());
+        me.setEmail(member.getEmail());
+        me.setAge(member.getAge());
+        memberRepository.save(me);
+        return me;
+    }
+
+    @PatchMapping("/members/{id}")
+    public Member patchMembers(@PathVariable("id") Long id, @RequestBody Member member) {
+        Member me =  memberRepository.findById(id).orElseThrow(NotFoundException::new);
+        if (member.getName() != null) me.setName(member.getName());
+        if (member.getEmail() != null) me.setEmail(member.getEmail());
+        if (member.getAge() != null) me.setAge(member.getAge());
+        memberRepository.save(me);
+        return me;
+    }
+
+    @DeleteMapping("/members/{id}")
+    public void deleteMembersById(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).orElseThrow(NotFoundException::new);
+        memberRepository.delete(member);
     }
 }
--- rest/src/main/java/com/example/demo/repository/MemberRepository.java
+++ rest/src/main/java/com/example/demo/repository/MemberRepository.java
@@ -4,6 +4,9 @@ import com.example.demo.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByName(String name);
 }
